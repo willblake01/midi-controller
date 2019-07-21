4cb3c843c76c403c84b87d3e5980e4e7b7a62922
William Blake <gwilliamblake@gmail.com>
2019-07-21 13:42:03 -0500

    initial commit

---
+++ presetHandler.js
@@ -0,0 +1,38 @@
+// Use this presets array inside your presetHandler
+const presets = require('./presets');
+
+const getPreset = (index) => {
+  return presets[index] || null;
+};
+
+const createOrUpdatePreset = (index, array) => {
+  if (!presets[index]) {
+    return;
+  }
+  presets[index] = array;
+  return presets[index];
+};
+
+// Complete this function:
+const presetHandler = (method, index, newPresetArray) => {
+  if (method === 'GET') {
+    let preset = getPreset(index);
+    if (preset) {
+      return [200, preset];
+    } else {
+      return [404];
+    }
+  } else if (method === 'PUT') {
+      const newPreset = createOrUpdatePreset(index, newPresetArray);
+      if (newPreset) {
+        return [200, newPreset];
+      } else {
+        return [404];
+      }
+    } else {
+      return [400];
+  }
+};
+
+// Leave this line so that your presetHandler function can be used elsewhere:
+module.exports = presetHandler;
